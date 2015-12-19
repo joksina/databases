0247365832db54ae6edc338da3fe16e3d7d788f3
joksina <jksalaudeen@yahoo.com>
2015-12-18 16:12:39 -0800

    tables are created

--- server/schema.sql
+++ server/schema.sql
@@ -1,14 +1,23 @@
-CREATE DATABASE chat;
+-- CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+CREATE TABLE users (
+  Id INT NOT NULL AUTO_INCREMENT,
+  username varchar(30) NOT NULL,
+  PRIMARY KEY(Id)
 );
 
-/* Create other tables and define schemas for them here! */
 
-
+CREATE TABLE messages (
+  /* Describe your table here.*/
+  id INT NOT NULL AUTO_INCREMENT,
+  texts varchar(125) NOT NULL,
+  username varchar(30) NOT NULL,
+  userId INT NOT NULL,
+  PRIMARY KEY(id),
+  FOREIGN KEY(userId) REFERENCES users(Id)
+);
 
 
 /*  Execute this file from the command line by typing:
