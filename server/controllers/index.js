d98280a9d1a3195df2b65d94d9e99e0e9667bab0
joksina <jksalaudeen@yahoo.com>
2015-12-18 23:12:57 -0800

    refactored sequelize controller

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -10,19 +10,14 @@ module.exports = {
         if(err){
           throw err;
         } else {
-          res.JSON(results);
+          res.status(200).json(results);
         }
       });
     }, // a function which handles a get request for all messages
     post: function (req, res) {
       var data = [req.body.username, req.body.message, req.body.roomname];
-      models.messages.post(data, function(err, results){
-        if(err) {
-          console.log(err);
-        } else {
-          res.JSON(results);
-        }
-      });
+      models.messages.post(data);
+          res.status(200).end();
     } // a function which handles posting a message to the database
   },
 
@@ -31,13 +26,46 @@ module.exports = {
     get: function (req, res) {
       models.users.get(function(err, results){
         if (err) { throw err; }
-        res.JSON(results);
+        res.status(200).json(results);
       });
     },
     post: function (req, res) {
       var data = [req.body.username];
-      models.users.post(data, function(err, results){
-        res.JSON(results);
+      models.users.post(data);
+        res.status(200).end();
+    }
+  }
+};
+
+//REFACTOR SEQUELIZE
+module.exports = {
+  messages: {
+    get: function (req, res) {
+      Messages.findAll({include: [User]}).complete(function(err, results) {
+        res.json(results);
+      });
+    },
+    post: function(req, res) {
+      User.findOrCreate({username: req.body.username}).complete(function(err, user){
+        user.id
+
+        var data = {text: req.body.text, userId: user.id, roomname: req.body.roomname};
+        Messages.create(data).complete(function(err, results){
+          res.sendStatus(201);
+        });
+      });
+    }
+  },
+
+  users: {
+    get: function(req, res) {
+      User.findAll().complete(function(err, results) {
+        res.json(results);
+      });
+    },
+    post: function(req, res) {
+      User.create({username: req.body.username}).complete(function(err, user){
+        res.sendStatus(201);
       });
     }
   }
